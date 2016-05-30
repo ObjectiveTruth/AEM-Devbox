a140ccbe73548a032647f0ca947d80c855a5911f
ObjectiveTruth <miguel@objectivetruth.ca>
2016-05-30 14:07:28 -0400

    Updated install_aem_package to include an alternative search string
    Good for when the package name is different than the filename

--- aem_install_files/resources/install_aem_package.sh
+++ aem_install_files/resources/install_aem_package.sh
@@ -1,19 +1,24 @@
 #! /bin/sh
 # Simple script to check if a package exists and if not, installs it to AEM
+# Uses the filename to search for the package name unless -s option is declared
 
-usage() { echo "Usage: $0 [-f <aem_package_to_install>] [-u <aem_url>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-f <aem_package_to_install>] [-u <aem_url>] [-s <aem_package_to_search_for_instead_of_filename(optional)>]" 1>&2; exit 1; }
 
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 package_file=""
 aem_url=""
+package_file_search=""
 
-while getopts ":f:u:" o; do
+while getopts ":f:u:s:" o; do
     case "${o}" in
         f)
             package_file=${OPTARG}
             ;;
         u)
             aem_url=${OPTARG}
+            ;;
+        s)
+            package_file_search=${OPTARG}
             ;;
         *)
             usage
@@ -26,9 +31,14 @@ if [ -z "${package_file}" ] || [ -z "${aem_url}" ]; then
     usage
 fi
 
-# Sanity checks done...
+# If -s option isn't specified, use package filename
+if [ -z "${package_file_search}" ]; then
+    package_file_search=$(basename "${package_file}")
+fi
 
-if [[ $(curl --fail -sS -u admin:admin ${aem_url}/crx/packmgr/service.jsp?cmd=ls) != *$(basename "${package_file}")* ]]; 
+# Sanity checks done, now the real work...
+
+if [[ $(curl --fail -sS -u admin:admin ${aem_url}/crx/packmgr/service.jsp?cmd=ls) != *${package_file_search}* ]]; 
     then 
         set -e # Turn on failing if a command returns non-zero exit code
         curl --fail -sS -u admin:admin -F file=@"${package_file}" -F force=true -F install=true ${aem_url}/crx/packmgr/service.jsp 
